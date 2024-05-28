52ac9b13396e00db60ed36bf430b97f7ea87e4d3
orchidHYE <trolly00@naver.com>
2024-05-28 23:41:56 +0900

    onebite-react: Node.js

---
+++ React/onebite-react/section03/src/math.js
@@ -0,0 +1,24 @@
+// math 모듈
+
+export function add(a, b) {
+  return a + b;
+}
+
+export function sub(a, b) {
+  return a - b;
+}
+
+export default function multiply(a, b) {
+  return a * b;
+}
+
+// ES Modules
+//export { add, sub };
+
+// CommonJS
+/*
+module.exports = {
+  add,
+  sub,
+};
+*/
