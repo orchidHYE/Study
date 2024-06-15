666931ef741b6f84ee7860dbda751bf00a36e7d2
orchidHYE <trolly00@naver.com>
2024-06-15 17:53:58 +0900

    투두리스트를 위한 셋팅

---
+++ React/onebite-react/section08/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./components/Header";
+import Editor from "./components/Editor";
+import List from "./components/List";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Editor />
+      <List />
+    </>
+  );
+}
+
+export default App;
