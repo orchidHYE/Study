6a193b3bf0fa84ee789ef806bdb530c01b76abb2
orchidHYE <trolly00@naver.com>
2024-07-06 23:43:31 +0900

    onebite-react: 투두리스트 기능 구현

--- React/onebite-react/section08/src/components/Editor.jsx
+++ React/onebite-react/section08/src/components/Editor.jsx
@@ -1,5 +1,41 @@
-const Editor = () => {
-  return <div>Editor</div>;
+import "./Editor.css";
+import { useState, useRef } from "react";
+
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+
+  const OnChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onSubmit = () => {
+    if (content === "") {
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent("");
+  };
+
+  const onkeydown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+
+  return (
+    <div className="Editor">
+      <input
+        ref={contentRef}
+        value={content}
+        onKeyDown={onkeydown}
+        onChange={OnChangeContent}
+        placeholder="새로운 Todo..."
+      />
+      <button onClick={onSubmit}>추가</button>
+    </div>
+  );
 };
 
 export default Editor;
--- React/onebite-react/section08/src/components/Header.jsx
+++ React/onebite-react/section08/src/components/Header.jsx
@@ -1,5 +1,12 @@
+import "./Header.css";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="Header">
+      <h3>오늘은 🗓️</h3>
+      <h1>{new Date().toDateString()}</h1>
+    </div>
+  );
 };
 
 export default Header;
